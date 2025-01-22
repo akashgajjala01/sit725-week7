88ba2c24fabbd913d6a34e1a0b7dc8376c5f0415
MJahanzaibKhan <programmerjahanzaib7@gmail.com>
2025-01-22 15:35:30 +1100

    code fixed

--- dbconnection.js
+++ dbconnection.js
@@ -1,52 +1,54 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 
-const uri = "mongodb+srv://akash:Akash%40123@cluster0.oybkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri =
+  "mongodb+srv://akash:Akash%40123@cluster0.oybkw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 let client;
 let collection;
 
-
 const connectDatabase = async () => {
-    try {
-        
-        client = await MongoClient.connect(uri);
-        console.log("Connected to MongoDB");
+  try {
+    // Connect to MongoDB
+    client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Connected to MongoDB");
 
-        
-        collection = client.db().collection('tech'); 
+    // Get the collection
+    collection = client.db().collection("tech");
 
-        
-        const count = await collection.countDocuments();
-        if (count === 0) {
-            await collection.insertMany([
-                {
-                    title: "javascript",
-                    image: "images/javascript.jpeg",
-                    link: "About JavaScript",
-                    description: "JavaScript is the programming language of the Web."
-                },
-                {
-                    title: "CSS",
-                    image: "images/css,jpeg",
-                    link: "About CSS",
-                    description: "CSS is the language we use to style an HTML document."
-                }
-            ]);
-            console.log("data added into MongoDB");
-        }
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        throw error; 
+    // Check if the collection has any documents
+    const count = await collection.countDocuments();
+    if (count === 0) {
+      // Insert data if collection is empty
+      await collection.insertMany([
+        {
+          title: "javascript",
+          image: "images/javascript.jpeg",
+          link: "About JavaScript",
+          description: "JavaScript is the programming language of the Web.",
+        },
+        {
+          title: "CSS",
+          image: "images/css.jpeg", // Fixed typo here
+          link: "About CSS",
+          description: "CSS is the language we use to style an HTML document.",
+        },
+      ]);
+      console.log("Data added into MongoDB");
     }
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    throw error;
+  }
 };
 
-
 const getClient = () => {
-    if (!client) {
-        throw new Error('MongoDB connection not established');
-    }
-    return client;
+  if (!client) {
+    throw new Error("MongoDB connection not established");
+  }
+  return client;
 };
 
 module.exports = { connectDatabase, getClient };
--- server.js
+++ server.js
@@ -2,38 +2,55 @@ const express = require("express");
 const http = require("http");
 const { connectDatabase } = require("./dbconnection");
 const cardRouter = require("./routers/routers");
-const { Server} = require("socket.io");
+const { Server } = require("socket.io");
 const app = express();
 const port = process.env.PORT || 5503;
 const server = http.createServer(app);
-const io = new Server({cors: {origin: "http://127.0.0.1:5503" }});
+
+// Updated CORS configuration for Socket.IO
+const io = new Server(server, {
+  cors: {
+    origin: "http://127.0.0.1:5503", // Make sure this matches your frontend origin
+    methods: ["GET", "POST"],
+  },
+});
 
 connectDatabase()
   .then(() => {
-    app.use(express.static(__dirname + '/public'));
+    app.use(express.static(__dirname + "/public"));
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
 
-    app.use('/api/tech', cardRouter);
+    // Handle new socket connections
+    io.on("connection", (socket) => {
+      console.log("A user connected");
 
-    io.on('connection', (socket) => {
-      console.log('a user connected');
-      
-      socket.on('disconnect', () => {
-        console.log('user disconnected');
+      // Listen for disconnect event
+      socket.on("disconnect", () => {
+        console.log("User disconnected");
       });
-  
+
+      // Example of sending data to the frontend
       setInterval(() => {
-        socket.emit('number', parseInt(Math.random() * 10));
+        socket.emit("number", parseInt(Math.random() * 10)); // Sending random number every 1 second
       }, 1000);
     });
 
+    // Middleware to make io available in routes
+    app.use((req, res, next) => {
+      req.io = io; // Attach io to request object
+      next();
+    });
+
+    // Use your routes (e.g., API routes)
+    app.use("/api/tech", cardRouter);
+
+    // Start the server
     server.listen(port, () => {
       console.log(`Server is running on port ${port}`);
-
     });
   })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
-    process.exit(1); 
+    process.exit(1);
   });
